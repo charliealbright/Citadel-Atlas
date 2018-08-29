e0087db828eabcf62e49180cad634217b04b9cb3
Charlie Albright <charlie.albright@aa.com>
2018-08-29 15:44:01 -0500

    Using new starter proj

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Citadel-Atlas
-An interactive map for the game Citadel: Forged with Fire
+# leaflet-js-demo
+
+A Node.js app using [Express 4](http://expressjs.com/) demonstrating Leaflet.js.
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
+
+```sh
+git clone git@github.com:mappingmtl/leaflet-js-demo.git # or clone your own fork
+cd leaflet-js-demo
+npm install
+npm start
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Deploying to Heroku
+
+```
+heroku create
+git push heroku master
+heroku open
+```
+
+Alternatively, you can deploy your own copy of the app using the web-based flow:
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Documentation
+
+For more information about using Node.js on Heroku, see these Dev Center articles:
+
+- [10 Habits of a Happy Node Hacker](https://blog.heroku.com/archives/2014/3/11/node-habits)
+- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
+- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
+- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
+- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
+# leaflet-js-demo
--- index.js
+++ index.js
@@ -1,10 +1,78 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Mongoose import
+var mongoose = require('mongoose');
+
+// Mongoose connection to MongoDB
+mongoose.connect('mongodb://user0:password0@ds153460.mlab.com:53460/mappingmtl', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
+// Mongoose Schema definition
+var Schema = mongoose.Schema;
+var JsonSchema = new Schema({
+    name: String,
+    type: Schema.Types.Mixed
+});
+ 
+// Mongoose Model definition
+var Json = mongoose.model('JString', JsonSchema, 'layer_collection');
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+var path = require ('path');
+app.use(express.static(path.join(__dirname + '/public')));
+
+// views is directory for all template files
+app.set('views', path.join(__dirname + '/views/pages'));
+app.set('view engine', 'pug');
+//app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+//routes/index.js
+/* GET layers json data. */
+app.get('/maplayers', function (req, res) {
+    Json.find({},{'name': 1}, function (err, docs) {
+        res.json(docs);
+    });
+});
+
+/* GET home page. */
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET json data. */
+app.get('/mapjson/:name', function (req, res) {
+    if (req.params.name) {
+        Json.findOne({ name: req.params.name },{}, function (err, docs) {
+            res.json(docs);
+        });
+    }
+});
+ 
+/* GET Map page. */
+app.get('/map', function(req,res) {
+    var db = req.db;
+    Json.find({},{}, function(err,docs){
+        res.render('map', {
+            "jmap" : docs,
+            lat : 45.501552,
+            lng : - 73.593094
+        });
+    });
+});
+
+module.exports = app;
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
